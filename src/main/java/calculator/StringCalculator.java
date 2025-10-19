63e2eea0e84aa6c318bd89406074624b0f6e97d3
eun-seoo <kimeunseo040@gamil.com>
2025-10-20 01:13:59 +0900

    feat(split): 커스텀 구분자 지정 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,4 +4,11 @@ public class StringCalculator {
     public boolean isEmptyInput(String input){
         return input == null || input.isEmpty();
     }
+
+    public String[] split(String input) {
+        String delimiter = ",|:";
+        // 슷자 부분을 구분자로 split -> tokens에는 숫자 문자열만 남도록
+        String[] tokens = splitNumbers.split(delimiter);
+        return tokens;
+    }
 }
