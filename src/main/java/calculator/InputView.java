bf73c708fe8a5083b18467a9ee61357c868ae664
eun-seoo <kimeunseo040@gamil.com>
2025-10-20 01:00:56 +0900

    feat(calc): 빈 문자열 또는 null 입력 시 0 반환 기능 구현

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -6,8 +6,8 @@ import java.lang.*;
 public class InputView {
     public String readInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-
         String input = Console.readLine();
-        return input;
+        // 사용자가 입력한 \\n을 실제 줄바꿈 문자로 변환
+        return input.replace("\\n", "\n");
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,7 @@
 package calculator;
 
 public class StringCalculator {
+    public boolean isEmptyInput(String input){
+        return input == null || input.isEmpty();
+    }
 }
