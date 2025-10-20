20eb1bba57b64a5ccc9885e0530a433b3ab991e0
eun-seoo <kimeunseo040@gamil.com>
2025-10-20 10:44:09 +0900

    feat(main): 문자열 계산기 애플리케이션 진입점 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,17 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        StringCalculator calculator = new StringCalculator();
+        OutputView outputView = new OutputView();
+
+        // 예외 처리
+        try{
+            String input = inputView.readInput();
+            int sum = calculator.calculator(input);
+            outputView.output(sum);
+        }catch(IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+        }
     }
 }
