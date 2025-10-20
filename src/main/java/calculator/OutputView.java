7dc774a0224552372401788bee9ea42556856185
eun-seoo <kimeunseo040@gamil.com>
2025-10-20 10:40:08 +0900

    feat(output): 결과 및 오류 메세지 출력 기능 구현

--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -1,4 +1,13 @@
 package calculator;
 
+import java.lang.*;
+
 public class OutputView {
+    public void output(int sum){
+        System.out.println("결과 : " + sum);
+    }
+
+    public void printError(String message){
+        System.out.println("오류 : " + message);
+    }
 }
